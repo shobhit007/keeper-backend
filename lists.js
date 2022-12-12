7c0aa348e9e0d38b11e66ad90ec7950f8840148c
shobhitsaini <shobhitsaini709@gmail.com>
2022-12-12 14:56:40 +0530

    update backend api

--- lists.js
+++ lists.js
@@ -4,39 +4,37 @@ import List from "./lists.model.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    List.find(function(err, lists){
-        if(!err){
-            res.send(lists);
-        }else {
-            res.send(err);
-        }
-    });
+  List.find(function (err, lists) {
+    if (!err) {
+      res.send(lists);
+    } else {
+      res.send(err);
+    }
+  });
 });
 
-router.post("/add", (req, res) => {
-    const list = new List({
-        title: req.body.title,
-        content: req.body.content
-    });
+router.post("/add", async (req, res) => {
+  const list = new List({
+    title: req.body.title,
+    content: req.body.content,
+  });
 
-    list.save(err => {
-        if (!err){
-            res.send("List successfully added to database.");
-        }else {
-            res.send(err);
-        }
-    });
+  try {
+    await list.save();
 
+    const lists = await List.find({});
+    res.send(lists);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-    List.findByIdAndDelete(req.params.id, function(err){
-        if (!err){
-            res.send("Successfully deleted.");
-        }else {
-            res.send(err);
-        }
-    });
+router.delete("/:id", async (req, res) => {
+  try {
+    await List.findByIdAndDelete(req.params.id);
+    const lists = await List.find({});
+    res.send(lists);
+  } catch (error) {}
 });
 
 export default router;
